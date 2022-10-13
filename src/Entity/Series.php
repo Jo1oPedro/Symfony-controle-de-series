1b51b497c7bf993c1ba483ec8939059d00e29850
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-13 01:45:37 -0300

    adiciona model/repository e migration para series

---
+++ src/Entity/Series.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeriesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeriesRepository::class)]
+class Series
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    public function __construct(
+        #[ORM\Column(type: 'string', length: 255)]
+        private string $name
+    )
+    {
+        return;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
